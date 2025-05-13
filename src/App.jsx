562f576944f6b6cfcc30852d05539986c0dc077b
Pratik <pgumthanavar05@gmail.com>
2025-05-13 18:05:01 +0530

    Add Redux for state management and implement login functionality
    - Integrated Redux Toolkit with appStore and userSlice for state management.
    - Updated App component to include Redux Provider and Feed component.
    - Enhanced Login component with axios for API calls and user authentication.
    - Modified Navbar to display user information conditionally.
    - Created Feed component for displaying user feeds.
    - Added BASE_URL constant for API endpoint configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,36 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Body from './components/body.jsx';
 import Login from './components/Login.jsx';
 import Profile from './components/Profile.jsx';
+import appStore from './redux/appStore.jsx';
+import { Provider } from 'react-redux';
+import Feed from './components/Feed.jsx';
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route
-            path="/"
-            element={<Body />}
-          >
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
             <Route
-              path="/login"
-              element={<Login />}
-            />
-            <Route
-              path="/profile"
-              element={<Profile />}
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+              path="/"
+              element={<Body />}
+            >
+              <Route
+                path="/"
+                element={<Feed />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../redux/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('pratik@gmail.com');
+  const [password, setPassword] = useState('Pratik@12');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        BASE_URL + '/login',
+        { email, password },
+        {
+          withCredentials: true,
+        }
+      );
+      if (response) {
+        dispatch(setUser(response.data));
+      }
+      navigate('/');
+    } catch (error) {
+      console.error('Login failed:', error.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center py-8">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          className="input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          className="input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {/* {error && <p className="text-red-500 mt-2">{error}</p>} */}
+        <button
+          className="btn btn-neutral mt-4"
+          onClick={() => handleLogin()}
+        >
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;
